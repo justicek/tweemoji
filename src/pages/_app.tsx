fc12e8cb81336ebe741487c2c3a71699aa22f051
Justice, Kevin <piek@tweemoji.com>
2023-05-02 11:55:12 -0400

    add auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { ClerkProvider } from "@clerk/nextjs";
 import { type AppType } from "next/app";
 
 import { api } from "y/utils/api";
@@ -5,7 +6,11 @@ import { api } from "y/utils/api";
 import "y/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ClerkProvider {...pageProps}>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
